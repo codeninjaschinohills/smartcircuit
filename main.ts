f81996fedee64460c78380f5c62ddc5323390c69
Jacob Jarecki <jsjwooowooo@gmail.com>
2021-10-06 13:31:12 -0700

    Added helper functions with comments to implement checking for solutions. Needs to be fully hooked up.

--- main.ts
+++ main.ts
@@ -40,12 +40,34 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         testBlock.x = curBlock.x
         testBlock.y = curBlock.y
         placedBlocks.push(testBlock)
+        let layoutPos = blockToLayout(testBlock.x, testBlock.y);
+        //console.log(`sprite position=${testBlock.x},${testBlock.y}`);
+        //console.log(`layout position=${layoutPos.x},${layoutPos.y}`);
+
+        let wireWidth = 4;
+        for (let i = 0; i < wireWidth; i++) {
+            currentLayout[layoutPos.y][layoutPos.x + i] += symbolWire;
+        }
+
+        //console.log(getLayoutString(currentLayout));
+
+        // symbolGoal = "G"
+        let positivePos = findSymbolInLayout(currentLayout, symbolPositive);
+        if (pathTo(currentLayout, positivePos.x, positivePos.y, symbolGoal)) {
+            // TODO: is this the only path we need to check?
+            // is this the only spot we need to check to see if we won?
+            // Also add in logic for win codition
+            // console.log(`Found path from ${positivePos.x},${positivePos.y} to ${symbolGoal}`);
+        }
+
     }
     if (selectorPanel == true) {
         if (selectedButton == 1) {
             for (let i = 0; i <= placedBlocks.length - 1; i++) {
                 placedBlocks[i].destroy()
             }
+            // TODO: How do we clear the currentLayout?
+            // a hint is that there should be an easy way to reset it!
         }
     }
 })
@@ -120,3 +142,187 @@ scene.setBackgroundImage(assets.image`bg`)
 if (prePlacedBlocks == 1) {
     let blockOverlay = sprites.create(assets.image`placedBlocksOverlay`, 0);
 }
+
+
+let layoutHeight = 7;
+let layoutWidth = 10;
+
+let symbolPositive = '+';
+let symbolNegative = '-';
+let symbolWire = 'X';
+let symbolGoal = 'G';
+let symbolVisited = 'V';
+
+// Copy and paste the empty layout to setup your own layout
+let emptyLayout =
+    [
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ']
+    ];
+
+// This layout matches this level
+// Note how there can be two symbols in a spot, like a Goal and a Wire
+let startingLayout =
+    [
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+        ['  ', '-X', 'X ', 'X ', 'X ', 'X ', 'X ', 'X ', 'GX', 'G '],
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+        ['  ', '+X', 'X ', 'X ', 'X ', 'X ', '  ', '  ', 'G ', 'G '],
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ']
+    ];
+
+let currentLayout = copyLayout(startingLayout);
+
+
+// Returns true if there is a path by wire from 'startX', 'startY' layout coordinates
+// to a spot in 'layout' containing all the 'endingSymbols'
+function pathTo(layout: string[][], startX: number, startY: number, endingSymbols: string): boolean {
+    // Calls the helper function below with a copy of 'layout' so we don't modify original
+    return pathToHelper(copyLayout(layout), startX, startY, endingSymbols);
+}
+
+// The recursive function (meaning it calls itself) that determines if a path exists
+function pathToHelper(layout: string[][], x: number, y: number, endingSymbols: string): boolean {
+    // If we are off the grid in the x direction, then we are not at the goal
+    if (x < 0 || x >= layoutWidth) {
+        return false;
+    }
+
+    // If we are off the grid in the y direction, then we are not at the goal
+    if (y < 0 || y >= layoutHeight) {
+        return false;
+    }
+
+    let currentSymbols = layout[y][x];
+
+    // If we have checked this spot before, don't check again!
+    if (currentSymbols.indexOf(symbolVisited) != -1) {
+        return false;
+    }
+
+    // Mark this spot as visited (add a 'V')
+    layout[y][x] = currentSymbols + symbolVisited;
+
+    // If the spot doesn't have a wire, it isn't a part of a path 
+    if (currentSymbols.indexOf(symbolWire) == -1) {
+        return false;
+    }
+
+    // If this spot is our goal, then we made it!
+    if (hasSymbols(endingSymbols, currentSymbols)) {
+        return true;
+    }
+
+    // If we made it here, then we need to keep checking
+    // See if a path exists from any adjacent spot to our goal
+    // Remember we avoid rechecking spots by looking for the 'V'
+    // markings we add as we go. 
+    return pathToHelper(layout, x + 1, y, endingSymbols)
+        || pathToHelper(layout, x, y + 1, endingSymbols)
+        || pathToHelper(layout, x - 1, y, endingSymbols)
+        || pathToHelper(layout, x, y - 1, endingSymbols);
+}
+
+// Returns true if 'toCheck' contains all the characters found in 'required'
+function hasSymbols(required: string, toCheck: string): boolean {
+    for (let i = 0; i < required.length; i++) {
+        if (toCheck.indexOf(required[i]) == -1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Returns a copy of the 'layout' so you can make changes
+// without modifying the original
+function copyLayout(layout: string[][]): string[][] {
+    let copyOfLayout = [];
+    for (let y = 0; y < layoutHeight; y++) {
+        let row = [];
+        for (let x = 0; x < layoutWidth; x++) {
+            row.push(layout[y][x]);
+        }
+        copyOfLayout.push(row);
+    }
+    return copyOfLayout;
+}
+
+// Returns the coordinates of the first 'sym' that is found
+// Used to easily get coordinates of special spots like
+// the positive and negative portions of the cirucit
+function findSymbolInLayout(layout: string[][], sym: string) {
+    for (let y = 0; y < layoutHeight; y++) {
+        for (let x = 0; x < layoutWidth; x++) {
+            if (hasSymbols(sym, layout[y][x])) {
+                return { x: x, y: y };
+            }
+        }
+    }
+    return null;
+}
+
+// Converts a block's position on MakeCode screen to coordinates in a layout
+function blockToLayout(blockX: number, blockY: number) {
+    let gridSpacing = 16;
+    // The returned object has a .x and .y property
+    return {
+        x: (blockX - 32) / gridSpacing,
+        y: (blockY - 8) / gridSpacing
+    }
+}
+
+// Takes a 'layout' and returns a string that can be printed for debugging
+function getLayoutString(layout: string[][]) {
+    let layoutString = '[\n';
+    for (let y = 0; y < layoutHeight; y++) {
+        layoutString += `${y}[`
+        for (let x = 0; x < layoutWidth; x++) {
+            layoutString += `'${padString(layout[y][x], 4)}', `
+        }
+        layoutString += ']\n'
+    }
+    layoutString += ']\n'
+    return layoutString;
+}
+
+// A helper function that pads a string 's' with 'n' underscores
+// Ensures spacing looks good for getLayoutString function
+// Underscores were used because spaces don't print to the console well
+function padString(s: string, n: number) {
+    let newString = s;
+    newString = newString.replaceAll(' ', '_');
+    while (newString.length < n) {
+        newString += '_';
+    }
+    return newString.split('').sort().join('');;
+}
+
+
+/*
+let testLayout =
+    [
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+        ['  ', '-X', 'X ', 'X ', 'X ', 'X ', 'X ', 'X ', 'GX', 'G '],
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+        ['  ', '+X', 'X ', 'X ', 'X ', 'X ', '  ', '  ', 'G ', 'G '],
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+        ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ']
+    ];
+
+console.log(getLayoutString(testLayout));
+let pos = findSymbolInLayout(testLayout, '+');
+if (pathTo(startingLayout, pos.x, pos.y, symbolGoal)) {
+    console.log("A PATH WAS FOUND!");
+}
+else {
+    console.log("A PATH WAS NOT FOUND!")
+}
+*/
