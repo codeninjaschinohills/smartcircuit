1731c6a75719f5550509e25d3331f24904254e61
Kulerdude <89493005+Kulerdude@users.noreply.github.com>
2021-10-09 13:47:09 -0700

    i didnt change anything lol

---
+++ tilemap.g.ts
@@ -0,0 +1,15 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myTile":
+            case "tile1":return tile1;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
